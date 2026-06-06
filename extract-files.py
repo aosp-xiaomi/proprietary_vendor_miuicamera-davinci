f3fb224354fe737a627433a93586545f25df76cc
lemezohaib <zhbkhan644@gmail.com>
2026-06-06 18:22:43 +0500

    Revert "Bring back face detection libs"
    This reverts commit 1aaaf42c649c883de031a7db00099eea84d7ebdb.
    
    Signed-off-by: lemezohaib <zhbkhan644@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -34,10 +34,6 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libgui_shim_miuicamera.so'),
     'system/priv-app/MiuiCamera/MiuiCamera.apk': blob_fixup()
         .apktool_patch('patches'),
-    ('vendor/lib64/libFaceDetectpp-0.5.2.so', 'vendor/lib64/libfacedet.so'): blob_fixup()
-        .binary_regex_replace(b'libmegface.so', b'libfacedet.so')
-        .binary_regex_replace(b'libMegviiFacepp-0.5.2.so', b'libFaceDetectpp-0.5.2.so')
-        .binary_regex_replace(b'megviifacepp_0_5_2_model', b'facedetectpp_0_5_2_model'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
--- xiaomi/xiaomi-vendor.mk
+++ xiaomi/xiaomi-vendor.mk
@@ -5,12 +5,7 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/miuicamera-davinci/xiaomi
 
-PRODUCT_COPY_FILES += \
-    vendor/miuicamera-davinci/xiaomi/proprietary/vendor/etc/camera/facedetectpp_0_5_2_model:$(TARGET_COPY_OUT_VENDOR)/etc/camera/facedetectpp_0_5_2_model
-
 PRODUCT_PACKAGES += \
     libcamera_algoup_jni.xiaomi \
     libcamera_mianode_jni.xiaomi \
-    libFaceDetectpp-0.5.2 \
-    libfacedet \
     MiuiCamera
